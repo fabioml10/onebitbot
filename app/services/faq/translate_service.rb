df2173d4bcd6f2382e831d7ae584b4bac6a5159e
Fabio Muller <fabioml10@yahoo.com.br>
2020-01-15 23:17:02 -0300

    translate method

--- app/services/faq/translate_service.rb
+++ app/services/faq/translate_service.rb
@@ -7,15 +7,14 @@ module FaqModule
  
     def call
       "chamou o metodo"
-
-      return @word
       
       # return 'Informe uma palavra.' if @word == nil
 
-      # key = "trnsl.1.1.20200115T143401Z.c576aeaaa427e198.5da5b5f78ee0d26cb1147e8ee9c5d01a2aec91a2"
-      # url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=#{key}&text=#{@word}&lang=pt-en"
-      # res = RestClient.get url
-      # value = JSON.parse(res.body)['text']
+      key = "trnsl.1.1.20200115T143401Z.c576aeaaa427e198.5da5b5f78ee0d26cb1147e8ee9c5d01a2aec91a2"
+      url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=#{key}&text=#{@word}&lang=pt-en"
+      res = RestClient.get url
+      value = JSON.parse(res.body)
+      return value
 
       # response = "O termo #{@word} traduzido para o inglês é: #{value}"
       # return response
