c4a90d3f5b3c28a678a37740b43eeb5c56649aa0
Fabio Muller <fabioml10@yahoo.com.br>
2020-01-16 21:52:36 -0300

    english to portuguese translation

--- app/services/faq/translate_service.rb
+++ app/services/faq/translate_service.rb
@@ -11,8 +11,10 @@ module FaqModule
       return 'Informe uma palavra.' if @word == nil
       return 'Informe o idioma.' if @lang == nil
 
-      lang_param = nil
-      @lang.upcase.include?(["INGLÊS", "INGLES", "ENGLISH"]) ? lang_param = "pt-en" : lang_param = "en-pt"
+      lang_param = "pt-en"
+      if @lang[0,1].upcase == "P"
+        lang_param = "en-pt"
+      end
 
       key = "trnsl.1.1.20200115T143401Z.c576aeaaa427e198.5da5b5f78ee0d26cb1147e8ee9c5d01a2aec91a2"
       url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=#{key}&text=#{@word}&lang=#{lang_param}"
